d8f02b14b9c10bdefdf24c38fb0a66eefdcaffa2
Husnain Mehmood <husnainMehmood5290@gmail.com>
2024-11-27 10:24:17 +0500

    create folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Project with llama_cpp',
+    version='0.0.0',
+    author="Husnain Mehmood",
+    author_email="HusnainMehmood5290@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
